5243dfe59495782f563e7bd15dbf8d22ac10feed
Andrei5455 <euandrei335@gmail.com>
2026-02-02 14:20:53 +0200

    Implementare comanda ADD EVENT

--- src/main/java/org/example/Alert.java
+++ src/main/java/org/example/Alert.java
@@ -7,5 +7,10 @@ public class Alert {
     private String message;
     private String ipAddress;
 
-
+    public Alert(String type, String severity, String message, String ipAddress) {
+        this.type = AlertType.valueOf(type);
+        this.severity = Severity.valueOf(severity);
+        this.message = message;
+        this.ipAddress = ipAddress;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,120 +13,127 @@ public class Main {
     public static void main(String[] args) {
         Database database = Database.getInstance();
 
-        Path inputFilePath = Paths.get(args[1] + ".in");
-        Path outputFilePath = Paths.get(args[1] + ".out");
-        int lineIndex = 0;
-        try(Scanner sc = new Scanner(inputFilePath.toFile()) ; PrintWriter pw = new PrintWriter(outputFilePath.toFile())) {
-            if (sc.hasNextLine())
-                sc.nextLine();
-            while (sc.hasNextLine()) {
-                String line = sc.nextLine();
-                lineIndex++;
-                String[] tokens = line.split("\\|", -1);
-                switch (tokens[0]) {
-                    case "ADD SERVER":
-                        try {
-                            Location location = new Location.Builder(tokens[4]).setCity(tokens[5]).setAddress(tokens[6]).setLatitude(tokens[7]).setLongitude(tokens[8]).build();
-                            User owner = UserFactory.create(tokens[9], tokens[10], tokens[11], tokens[12], tokens[13]);
-                            Server server = new Server.Builder(tokens[2], location, owner).setHostName(tokens[1]).setStatus((ServerStatus.valueOf(tokens[3]))).setCpuCores(tokens[14]).setRamGb(tokens[15]).setStorageGb(tokens[16]).build();
-                            database.addServer(server);
-                            pw.println("ADD SERVER: " + tokens[2] + ": " + tokens[3]);
+        for (int i = 1; i < args.length; i++) {
+            Path inputFilePath = Paths.get(args[i] + ".in");
+            Path outputFilePath = Paths.get(args[i] + ".out");
+            int lineIndex = 0;
+            try (Scanner sc = new Scanner(inputFilePath.toFile()); PrintWriter pw = new PrintWriter(outputFilePath.toFile())) {
+                if (sc.hasNextLine())
+                    sc.nextLine();
+                while (sc.hasNextLine()) {
+                    String line = sc.nextLine();
+                    lineIndex++;
+                    String[] tokens = line.split("\\|", -1);
+                    switch (tokens[0]) {
+                        case "ADD SERVER":
+                            try {
+                                Location location = new Location.Builder(tokens[4]).setCity(tokens[5]).setAddress(tokens[6]).setLatitude(tokens[7]).setLongitude(tokens[8]).build();
+                                User owner = UserFactory.create(tokens[9], tokens[10], tokens[11], tokens[12], tokens[13]);
+                                Server server = new Server.Builder(tokens[2], location, owner).setHostName(tokens[1]).setStatus((ServerStatus.valueOf(tokens[3]))).setCpuCores(tokens[14]).setRamGb(tokens[15]).setStorageGb(tokens[16]).build();
+                                database.addServer(server);
+                                pw.println("ADD SERVER: " + tokens[2] + ": " + tokens[3]);
+                                break;
+                            } catch (MissingIpAddressException | UserException | LocationException e) {
+                                pw.print("ADD SERVER: " + e.getMessage());
+                                pw.println(" ## line no: " + lineIndex);
+                            }
                             break;
-                        } catch (MissingIpAddressException | UserException | LocationException e) {
-                            pw.print("ADD SERVER: " + e.getMessage());
-                            pw.println(" ## line no: " + lineIndex);
-                        }
-                        break;
-                    case "ADD GROUP":
-                        try {
-                            ResourceGroup group = new ResourceGroup(tokens[1]);
-                            database.addResourceGroup((group));
-                            pw.println("ADD GROUP: " + tokens[1]);
-                        } catch (MissingIpAddressException e) {
-                            pw.print("ADD GROUP: " + e.getMessage());
-                            pw.println(" ## line no: " + lineIndex);
-                        }
-                        break;
-                    case "FIND GROUP":
-                        try {
-                            if (database.findGroup(tokens[1]))
-                                pw.println("FIND GROUP: " + tokens[1]);
-                            else pw.println("FIND GROUP: Group not found: ipAddress = " + tokens[1]);
-                        } catch (MissingIpAddressException e) {
-                            pw.print("FIND GROUP: " + e.getMessage());
-                            pw.println(" ## line no: " + lineIndex);
-                        }
-                        break;
-                    case "REMOVE GROUP":
-                        try {
-                            if (!database.findGroup(tokens[1]))
-                                pw.println("REMOVE GROUP: Group not found: ipAddress = " + tokens[1]);
-                            else {
-                                pw.println("REMOVE GROUP: " + tokens[1]);
-                                database.removeResourceGroup(tokens[1]);
+                        case "ADD GROUP":
+                            try {
+                                ResourceGroup group = new ResourceGroup(tokens[1]);
+                                database.addResourceGroup((group));
+                                pw.println("ADD GROUP: " + tokens[1]);
+                            } catch (MissingIpAddressException e) {
+                                pw.print("ADD GROUP: " + e.getMessage());
+                                pw.println(" ## line no: " + lineIndex);
                             }
-                        } catch (MissingIpAddressException e) {
-                            pw.print("REMOVE GROUP: " + e.getMessage());
-                            pw.println(" ## line no: " + lineIndex);
-                        }
-                        break;
-                    case "ADD MEMBER":
-                        try {
-                            if (!database.findGroup(tokens[1]))
-                                pw.println("ADD MEMBER: Group was not found: ipAddress = " + tokens[1]);
-                            else {
-                                ResourceGroup targetGroup = database.getResourceGroup(tokens[1]);
-                                User member = UserFactory.create(tokens[2], tokens[3], tokens[4], tokens[5], tokens[6]);
-                                targetGroup.addMember(member);
-                                pw.println("ADD MEMBER: " + tokens[1] + ": name = " + tokens[2] + " && role = " + tokens[3]);
+                            break;
+                        case "FIND GROUP":
+                            try {
+                                if (database.findGroup(tokens[1]))
+                                    pw.println("FIND GROUP: " + tokens[1]);
+                                else pw.println("FIND GROUP: Group not found: ipAddress = " + tokens[1]);
+                            } catch (MissingIpAddressException e) {
+                                pw.print("FIND GROUP: " + e.getMessage());
+                                pw.println(" ## line no: " + lineIndex);
                             }
-                        } catch (MissingIpAddressException e) {
-                            pw.print("ADD MEMBER: " + e.getMessage());
-                            pw.println(" ## line no: " + lineIndex);
-                        }
-                        break;
-                    case "FIND MEMBER":
-                        try {
-                            if (!database.findGroup(tokens[1]))
-                                pw.println("FIND MEMBER: Group not found: ipAddress = " + tokens[1]);
-                            else {
-                                ResourceGroup targetGroup = database.getResourceGroup(tokens[1]);
-                                if (targetGroup.findMember(tokens[2], tokens[3]) != null) {
-                                    pw.println("FIND MEMBER: " + tokens[1] + ": name = " + tokens[2] + " && role = " + tokens[3]);
-                                } else {
-                                    pw.println("FIND MEMBER: Member not found: ipAddress = " + tokens[1] + ": name = " + tokens[2] + " && role = " + tokens[3]);
+                            break;
+                        case "REMOVE GROUP":
+                            try {
+                                if (!database.findGroup(tokens[1]))
+                                    pw.println("REMOVE GROUP: Group not found: ipAddress = " + tokens[1]);
+                                else {
+                                    pw.println("REMOVE GROUP: " + tokens[1]);
+                                    database.removeResourceGroup(tokens[1]);
                                 }
+                            } catch (MissingIpAddressException e) {
+                                pw.print("REMOVE GROUP: " + e.getMessage());
+                                pw.println(" ## line no: " + lineIndex);
                             }
-                        } catch (MissingIpAddressException | UserException e) {
-                            pw.print("FIND MEMBER: " + e.getMessage());
-                            pw.println(" ## line no: " + lineIndex);
-                        }
-                        break;
-                    case "REMOVE MEMBER":
-                        try {
-                            if (!database.findGroup(tokens[1]))
-                                pw.println("REMOVE MEMBER: Group not found: ipAddress = " + tokens[1]);
-                            else {
-                                ResourceGroup targetGroup = database.getResourceGroup(tokens[1]);
-                                User targetMember = targetGroup.findMember(tokens[2], tokens[3]);
-                                if (targetMember != null) {
-                                    pw.println("REMOVE MEMBER: " + tokens[1] + ": name = " + tokens[2] + " && role = " + tokens[3]);
-                                    targetGroup.removeMember(targetMember);
-                                } else {
-                                    pw.println("REMOVE MEMBER: Member not found: ipAddress = " + tokens[1] + ": name = " + tokens[2] + " && role = " + tokens[3]);
+                            break;
+                        case "ADD MEMBER":
+                            try {
+                                if (!database.findGroup(tokens[1]))
+                                    pw.println("ADD MEMBER: Group was not found: ipAddress = " + tokens[1]);
+                                else {
+                                    ResourceGroup targetGroup = database.getResourceGroup(tokens[1]);
+                                    User member = UserFactory.create(tokens[2], tokens[3], tokens[4], tokens[5], tokens[6]);
+                                    targetGroup.addMember(member);
+                                    pw.println("ADD MEMBER: " + tokens[1] + ": name = " + tokens[2] + " && role = " + tokens[3]);
                                 }
+                            } catch (MissingIpAddressException e) {
+                                pw.print("ADD MEMBER: " + e.getMessage());
+                                pw.println(" ## line no: " + lineIndex);
                             }
-                        } catch (MissingIpAddressException | UserException e) {
-                            pw.print("REMOVE MEMBER: " + e.getMessage());
-                            pw.println(" ## line no: " + lineIndex);
-                        }
-                    default:
-                        break;
+                            break;
+                        case "FIND MEMBER":
+                            try {
+                                if (!database.findGroup(tokens[1]))
+                                    pw.println("FIND MEMBER: Group not found: ipAddress = " + tokens[1]);
+                                else {
+                                    ResourceGroup targetGroup = database.getResourceGroup(tokens[1]);
+                                    if (targetGroup.findMember(tokens[2], tokens[3]) != null) {
+                                        pw.println("FIND MEMBER: " + tokens[1] + ": name = " + tokens[2] + " && role = " + tokens[3]);
+                                    } else {
+                                        pw.println("FIND MEMBER: Member not found: ipAddress = " + tokens[1] + ": name = " + tokens[2] + " && role = " + tokens[3]);
+                                    }
+                                }
+                            } catch (MissingIpAddressException | UserException e) {
+                                pw.print("FIND MEMBER: " + e.getMessage());
+                                pw.println(" ## line no: " + lineIndex);
+                            }
+                            break;
+                        case "REMOVE MEMBER":
+                            try {
+                                if (!database.findGroup(tokens[1]))
+                                    pw.println("REMOVE MEMBER: Group not found: ipAddress = " + tokens[1]);
+                                else {
+                                    ResourceGroup targetGroup = database.getResourceGroup(tokens[1]);
+                                    User targetMember = targetGroup.findMember(tokens[2], tokens[3]);
+                                    if (targetMember != null) {
+                                        pw.println("REMOVE MEMBER: " + tokens[1] + ": name = " + tokens[2] + " && role = " + tokens[3]);
+                                        targetGroup.removeMember(targetMember);
+                                    } else {
+                                        pw.println("REMOVE MEMBER: Member not found: ipAddress = " + tokens[1] + ": name = " + tokens[2] + " && role = " + tokens[3]);
+                                    }
+                                }
+                            } catch (MissingIpAddressException | UserException e) {
+                                pw.print("REMOVE MEMBER: " + e.getMessage());
+                                pw.println(" ## line no: " + lineIndex);
+                            }
+                            break;
+                        case "ADD EVENT":
+                            Alert alert = new Alert(tokens[1], tokens[2], tokens[4], tokens[3]);
+                            database.addAlert(alert);
+                            pw.println("ADD EVENT: " + tokens[3] + ": type = " + tokens[1] + " && severity = " + tokens[2] + " && message = " + tokens[4]);
+                        default:
+                            break;
+                    }
                 }
-            }
-        } catch (FileNotFoundException e) {
+            } catch (FileNotFoundException e) {
 
+            }
+            System.out.println(System.getProperty("user.dir"));
         }
-        System.out.println(System.getProperty("user.dir"));
     }
 }
